7336e92c2f7e11b9f2fc882440f58ca0691511e8
Ruslan Khomenko <37312893+Ruslan3584@users.noreply.github.com>
2020-10-11 22:34:16 +0300

    Update lab1.py
    added max() for second letter recognition and changed function for first letter

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[233]:
 
 
 import json
@@ -20,11 +19,9 @@ with open('frequencies.json') as json_file:
 
 
 
+
 alphabet_list = list(string.ascii_lowercase + ' ')
 alphabet_dict = dict((j,i) for i,j in enumerate(alphabet_list))
-
-
-
 
 array = np.zeros([len(alphabet_list),len(alphabet_list)]).astype('int')
 for i in alphabet_dict:
@@ -34,22 +31,14 @@ for i in alphabet_dict:
 
 
 
-
-
 p_k = (array.T/array.sum(axis=1)).T # p(k_i+1=h|k_i=p)
 p_k= np.log(p_k, out=np.full_like(p_k,-np.inf), where=(p_k!=0))
-
-
-
 
 etalones = {}
 for i in alphabet_list[:-1] + ['space']:
     img = np.array(Image.open(f'alphabet/{i}.png')).astype('int')
     etalones[i] = img
 etalones[' '] = etalones.pop('space')
-
-
-
 
 
 def string_to_image(string,noise_level):
@@ -61,52 +50,53 @@ def string_to_image(string,noise_level):
     ksi = np.random.binomial(size=n*m, n=1, p=noise_level).reshape(image.shape)
     return ksi^image
 
-
-
-
-p = 0.1
-string = string_to_image('an',p)
+p = 0.3
+string = string_to_image('ok',p)
 penalties = np.full([string.shape[1],len(alphabet_list)],-np.inf)
 #penalties = np.zeros([string.shape[1],len(alphabet_list)])
 #test = np.where(p_k==0, -np.inf, p_k) 
 
-etalones['a'].shape[1],etalones['n'].shape[1]#,etalones['g'].shape[1]
+etalones['o'].shape[1],etalones['k'].shape[1]#,etalones['g'].shape[1]
 
 
-
-
-#+ np.log(p_k[-1,alphabet_dict[etalone]])
 for etalone in etalones:
     ind = etalones[etalone].shape[1]
     if ind <= string.shape[1]:
         s =  string[:,:ind]
-        penalties[ind,alphabet_dict[etalone]] =             np.sum(np.log(p)*(s^etalones[etalone]) +
+        penalties[ind-1,alphabet_dict[etalone]] =             np.sum(np.log(p)*(s^etalones[etalone]) +
                    np.log(1-p)*(1^s^etalones[etalone]) +  p_k[-1,alphabet_dict[etalone]])
-
-
-
-
-np.argmax(penalties[20]), alphabet_list[np.argmax(penalties[20])]
-
 
 
 proper = np.where((penalties != -np.inf).any(axis=1))[0]
 
 
-
-penalties2 = np.full([string.shape[1],len(alphabet_list)],-np.inf)
 for x in proper:
     for etalone in etalones:
         ind = etalones[etalone].shape[1] + x
         if ind <= string.shape[1]:
             s =  string[:,x:ind]
-            penalties2[ind - 1,alphabet_dict[etalone]] =                 np.sum(np.log(p)*(s^etalones[etalone]) +
-                       np.log(1-p)*(1^s^etalones[etalone])  + p_k[np.argmax(penalties[x]),alphabet_dict[etalone]])
+            penalties[ind-1,alphabet_dict[etalone]] =  max(penalties[ind-1,alphabet_dict[etalone]],                     np.sum(np.log(p)*(s^etalones[etalone]) +
+                           np.log(1-p)*(1^s^etalones[etalone])) + 
+                                                            p_k[np.argmax(penalties[x-1]),alphabet_dict[etalone]])
 
 
 
-np.argmax(penalties2[-1])
+np.argmax(penalties[-1]), alphabet_list[np.argmax(penalties[-1])]
+
+np.argmax(penalties[-17]), alphabet_list[np.argmax(penalties[-17])]
 
 
+'''
+#penalties2 = np.full([string.shape[1],len(alphabet_list)],-np.inf)
+for x in proper:
+    for element in range(len(penalties[x])):
+        for etalone in etalones:
+            ind = etalones[etalone].shape[1] + x
+            if ind <= string.shape[1]:
+                s =  string[:,x:ind]
+                penalties[ind-1,alphabet_dict[etalone]] =  max(penalties[ind-1,alphabet_dict[etalone]], \
+                    (np.sum(np.log(p)*(s^etalones[etalone]) +
+                           np.log(1-p)*(1^s^etalones[etalone])) + p_k[element,alphabet_dict[etalone]]) )
+'''
 
 

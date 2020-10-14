962e9851de00df247d61d93453c3e0db172e7be3
Maksym Shylo <33497690+maksymshylo@users.noreply.github.com>
2020-10-15 00:09:05 +0300

    rewritten  recognizing second letter algorithm
    Now, it's fully working code for recognizing word with two letters

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
 import json
 import numpy as np
 import string
 import matplotlib.pyplot as plt
 from PIL import Image
-import os 
-
+import os
 
 
 
@@ -18,10 +12,10 @@ with open('frequencies.json') as json_file:
 
 
 
-
-
 alphabet_list = list(string.ascii_lowercase + ' ')
 alphabet_dict = dict((j,i) for i,j in enumerate(alphabet_list))
+
+
 
 array = np.zeros([len(alphabet_list),len(alphabet_list)]).astype('int')
 for i in alphabet_dict:
@@ -31,8 +25,10 @@ for i in alphabet_dict:
 
 
 
-p_k = (array.T/array.sum(axis=1)).T # p(k_i+1=h|k_i=p)
-p_k= np.log(p_k, out=np.full_like(p_k,-np.inf), where=(p_k!=0))
+p_k = (array.T/array.sum(axis=1)).T 
+p_k= np.log(p_k, out=np.full_like(p_k, -np.inf), where=(p_k!=0))
+
+
 
 etalones = {}
 for i in alphabet_list[:-1] + ['space']:
@@ -41,62 +37,72 @@ for i in alphabet_list[:-1] + ['space']:
 etalones[' '] = etalones.pop('space')
 
 
+
+
 def string_to_image(string,noise_level):
     image = etalones[string[0]]
     for i in string[1:]:
         image = np.hstack([image,etalones[i]])
     n,m = image.shape
-    #t = len(string)
-    ksi = np.random.binomial(size=n*m, n=1, p=noise_level).reshape(image.shape)
-    return ksi^image
-
-p = 0.3
-string = string_to_image('ok',p)
-penalties = np.full([string.shape[1],len(alphabet_list)],-np.inf)
-#penalties = np.zeros([string.shape[1],len(alphabet_list)])
-#test = np.where(p_k==0, -np.inf, p_k) 
-
-etalones['o'].shape[1],etalones['k'].shape[1]#,etalones['g'].shape[1]
-
-
-for etalone in etalones:
-    ind = etalones[etalone].shape[1]
-    if ind <= string.shape[1]:
-        s =  string[:,:ind]
-        penalties[ind-1,alphabet_dict[etalone]] =             np.sum(np.log(p)*(s^etalones[etalone]) +
-                   np.log(1-p)*(1^s^etalones[etalone]) +  p_k[-1,alphabet_dict[etalone]])
-
-
-proper = np.where((penalties != -np.inf).any(axis=1))[0]
-
-
-for x in proper:
-    for etalone in etalones:
-        ind = etalones[etalone].shape[1] + x
-        if ind <= string.shape[1]:
-            s =  string[:,x:ind]
-            penalties[ind-1,alphabet_dict[etalone]] =  max(penalties[ind-1,alphabet_dict[etalone]],                     np.sum(np.log(p)*(s^etalones[etalone]) +
-                           np.log(1-p)*(1^s^etalones[etalone])) + 
-                                                            p_k[np.argmax(penalties[x-1]),alphabet_dict[etalone]])
+    if noise_level ==0:
+        return image
+    else: 
+        ksi = np.random.binomial(size=n*m, n=1, p=noise_level).reshape(image.shape)
+        return ksi^image
 
 
 
-np.argmax(penalties[-1]), alphabet_list[np.argmax(penalties[-1])]
+p = 0.4
+string = string_to_image('my',p) # генеруємо рядок
+# генеруємо масив з -np.inf шейпом (ширина рядка х алфавіт)
+penalties = np.full([string.shape[1]+1,len(alphabet_list)],-np.inf)
+letters = list(etalones.values())
 
-np.argmax(penalties[-17]), alphabet_list[np.argmax(penalties[-17])]
+#etalones['o'].shape[1],etalones['f'].shape[1]
+
+# first letter
+q = []
+for i,letter in enumerate(letters):
+    cut_till = letter.shape[1]
+    if cut_till <= string.shape[1]:
+        q.append(cut_till)
+        x = string[:,:cut_till]
+        f = np.sum( (x^letter)*np.log(p) + (1^x^letter)*np.log(1-p)   ) + p_k[-1, i]
+        penalties[cut_till,i] = f
+        
+q = list(set(q))
 
 
-'''
-#penalties2 = np.full([string.shape[1],len(alphabet_list)],-np.inf)
-for x in proper:
-    for element in range(len(penalties[x])):
-        for etalone in etalones:
-            ind = etalones[etalone].shape[1] + x
-            if ind <= string.shape[1]:
-                s =  string[:,x:ind]
-                penalties[ind-1,alphabet_dict[etalone]] =  max(penalties[ind-1,alphabet_dict[etalone]], \
-                    (np.sum(np.log(p)*(s^etalones[etalone]) +
-                           np.log(1-p)*(1^s^etalones[etalone])) + p_k[element,alphabet_dict[etalone]]) )
-'''
+# second letter
+for first_el_len in q:
+    
+    for index_f, f_first_el in enumerate(penalties[first_el_len]):
+        
+        if f_first_el != -np.inf:
+            
+            for i,letter in enumerate(letters):
+                cut_till = letter.shape[1] + first_el_len 
+                if cut_till <= string.shape[1]:
+                    x = string[:,first_el_len:cut_till]
+                    f = np.sum( (x^letter)*np.log(p) + (1^x^letter)*np.log(1-p) ) + p_k[index_f, i] + f_first_el
+                    penalties[cut_till,i] = max(f , penalties[cut_till,i] )
+
+
+
+
+last_argmax = np.argmax(penalties[-1])
+last_letter = alphabet_list[last_argmax]
+print(last_argmax,last_letter)
+
+
+
+
+index_f = penalties.shape[0]-etalones[last_letter].shape[1]-1
+first_argmax =  np.argmax(penalties[index_f])
+print(first_argmax, alphabet_list[first_argmax] )
+
+
+
+
 
 
